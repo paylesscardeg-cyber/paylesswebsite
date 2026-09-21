0a5f0b7be5bb6bddd3230e7898481aaead6b6963
paylesscardeg-cyber <paylesscardeg@gmail.com>
2026-09-21 15:47:41 +0300

    Added .gitignore

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -91,7 +91,7 @@ const Footer: React.FC = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400 text-sm">
-            © 2025 Payless Travel. All rights reserved. | Terms of Service | Privacy Policy
+            © 2026 Payless Travel. All rights reserved. | Terms of Service | Privacy Policy
           </p>
         </div>
       </div>
